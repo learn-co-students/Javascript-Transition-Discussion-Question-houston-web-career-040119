a43cdb7695708801d016cb9670862c8f5ba82acb
github username <github email address>
2019-05-22 09:49:09 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
-}
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
+
+
+
+
+// function saveLincoln(interval) {
+//   if (interval = 1000) {
+
+//   }
+// }
+
+
+let btn = document.querySelector('#save_lincoln')
+let img = document.querySelector('#foreground')
+let interval = document.querySelector('#interval')
+btn.addEventListener('click', function(event){
+  img.src=''
+  // debugger
+})
